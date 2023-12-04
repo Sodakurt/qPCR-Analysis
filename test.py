6d1f334e483995f28ead60f081ebb7deb70200c6
Magnus Jungersen <magnusjungersen@gmail.com>
2023-12-04 12:39:13 +0100

    updates to initialization of data

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
 # Take data from same folder and output figures in same folder
+import csv
+import os
 
 # intialize data
+genes = ['GAPDH3', 'NANOG', 'NESTIN', 'OCT3/4', 'PAX6', 'SOX2'] 
+lines = ['H1', 'A4', 'D5']
 
-# more small changes
+with open(r'C:\Users\magnu\OneDrive - University of Copenhagen\3. år Bachelorprojekt\Code\qPCR\qPCR-Analysis\qPCR_result.csv', 'r') as csvfile: 
+    reader = csv.reader(csvfile)
+    for row in reader:
+        print(row)
+
+
+
 
 # data analysis
 
